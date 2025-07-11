e1314998f309d4211166ec066111de9789c215b4
adham <=adhambekboylanov@gmail.com>
2025-07-11 21:37:41 +0500

    kfwcnd

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'configapp',
+    'configapp1',
+    'configapp2',
 ]
 
 MIDDLEWARE = [
